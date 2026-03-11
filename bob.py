805be8bd7169a64e12f83d5a28286bff4745763b
Serero <projectsleeper9@gmail.com>
2026-03-11 14:58:53 +0200

    ipdated flask

--- bob.py
+++ bob.py
@@ -73,4 +73,4 @@ def chat():
 # Check if this script is being run directly (not imported as a module)
 if __name__ == "__main__":
      # Start the Flask development server with debug mode enabled (auto-reloads on code changes)
-     app.run(debug=True)
+     app.run(host="0.0.0.0", port=8000)
